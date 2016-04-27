3c512f43b04e4157d845e3b7acd6e5dd6e9b28b1
Dominique Barton <dbarton@confirm.ch>
2016-04-28 01:56:23 +0200

    setting delivery date on complete action

--- ordertracking/views.py
+++ ordertracking/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse
 from django.views.generic import View
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
+from datetime import date
 from .models import Order, Store, State
 
 
@@ -20,8 +21,12 @@ class OrderCompleteView(View):
 
     def get(self, request,  *args, **kwargs):
         order = get_object_or_404(Order, complete=False, pk=self.kwargs['pk'])
+
         order.complete = True
+        if not order.delivery_date:
+            order.delivery_date = date.today()
         order.save()
+
         if 'HTTP_REFERER' in request.META:
             return redirect(request.META['HTTP_REFERER'])
         else:
