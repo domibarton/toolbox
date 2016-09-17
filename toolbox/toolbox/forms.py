5893585ca601705610cf1541ae3725d8c63d1218
Dominique Barton <dbarton@confirm.ch>
2016-09-17 16:10:46 +0200

    BUGFIX: Fixed login page and missing CSS / compress files

--- toolbox/toolbox/forms.py
+++ toolbox/toolbox/forms.py
@@ -30,7 +30,7 @@ class ToolboxAuthenticationForm(AuthenticationForm):
     def __init__(self, *args, **kwargs):
         super(ToolboxAuthenticationForm, self).__init__(*args, **kwargs)
         self.helper = FormHelper()
-        self.helper.add_input(Submit('submit', 'login', css_class='btn success pull-right'))
+        self.helper.add_input(Submit('submit', 'login', css_class='btn green pull-right'))
 
 
 class ToolboxPasswordChangeForm(PasswordChangeForm):
@@ -38,4 +38,4 @@ class ToolboxPasswordChangeForm(PasswordChangeForm):
     def __init__(self, *args, **kwargs):
         super(ToolboxPasswordChangeForm, self).__init__(*args, **kwargs)
         self.helper = FormHelper()
-        self.helper.add_input(Submit('submit', 'change password', css_class='btn success'))
+        self.helper.add_input(Submit('submit', 'change password', css_class='btn green'))
