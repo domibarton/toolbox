893d912ee6825537d30a666f5c487f8bceb6c156
Dominique Barton <dbarton@confirm.ch>
2016-09-18 15:46:04 +0200

    DOCS: Removed docs from HACTAR

--- toolbox/toolbox/forms.py
+++ toolbox/toolbox/forms.py
@@ -1,24 +1,4 @@
 # -*- coding: utf-8 -*-
-'''
-Definition of all HACTAR core related forms.
-
-Django provides a lot of tools and libraries to work with data inputs received
-by the user. Forms are a big part of these libraries and they'll help you to
-render, validate and save forms based on models. More about working with Django
-forms can be found here:
-
-https://docs.djangoproject.com/en/1.9/topics/forms/
-
-Because we're using a Twitter Bootstrap theme we're not able to use Django's
-stock forms and we're using a great alternative called Crispy Forms. With
-Crispy Forms we can render forms in a Bootstrap-compliant way and even enhance
-them to our desired look and feel. More about Crispy Forms here:
-
-http://django-crispy-forms.readthedocs.org/en/latest/
-
-Copyright © 2016 confirm IT solutions
-'''
-
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit
 from django.forms import ModelForm, PasswordInput
