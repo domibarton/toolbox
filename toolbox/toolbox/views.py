a841c9f8ce8ada901f7b0173169eacb34efa6283
Dominique Barton <dbarton@confirm.ch>
2016-09-20 15:41:29 +0200

    FEATURE: Added "coming soon" orders to home page

--- toolbox/toolbox/views.py
+++ toolbox/toolbox/views.py
@@ -1,5 +1,18 @@
-from django.views.generic import TemplateView
+from django.views.generic.list import ListView
+from ordertracking.models import Order
+from django.utils import timezone
+from datetime import timedelta
 
 
-class HomeView(TemplateView):
+class HomeView(ListView):
+    '''
+    Home view which displays orders which should be coming soon (shipped in
+    over or equals 10 days).
+    '''
     template_name = 'toolbox/home.html'
+    model = Order
+
+    def get_queryset(self):
+        ts = timezone.now() - timedelta(days=10)
+        qs = super(HomeView, self).get_queryset()
+        return qs.filter(complete=False, delivery_date=None, shipping_date__lte=ts).order_by('shipping_date')
